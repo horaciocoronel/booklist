80c50e060713ca011a4861eeca7b01bdabadd831
horaciocoronel <arielc@gmail.com>
2017-11-03 12:33:56 -0400

    Add html and text view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,11 @@
 class BooksController < ApplicationController
-
+  def index
+    @books = Book.all
+    
+    respond_to do |format|
+      format.html #html is the format this action will respond to
+      format.text
+    end
+  end
 
 end
