b9987b3031b79e75a23fbe747a156ff73f753069
horaciocoronel <arielc@gmail.com>
2017-11-03 12:39:21 -0400

    Add CSV view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,10 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
-    
+
     respond_to do |format|
       format.html #html is the format this action will respond to
+      format.csv
       format.text
     end
   end
