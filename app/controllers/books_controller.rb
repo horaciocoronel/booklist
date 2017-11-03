61db8500408c99e095f92edd39b4f465472c592b
horaciocoronel <arielc@gmail.com>
2017-11-03 14:40:42 -0400

    Add CSV format

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,8 +4,20 @@ class BooksController < ApplicationController
 
     respond_to do |format|
       format.html #html is the format this action will respond to
-      format.csv
+      format.csv do
+        csv_data = Book.generate_csv(@books)
+        render plain: csv_data
+      end
       format.text
+      format.json do
+        json_data = @books.map {|book|
+          {title: book.title, author: book.author, already_read: book.already_read}
+        }
+        # render json: json_data
+        render json: { books: json_data,
+                       time: Time.now,
+                       count: @books.count }
+        end
     end
   end
 
